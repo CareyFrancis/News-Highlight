096686ee22150c13dde20749ff50d5eaf4115921
CareyFrancis <carexfm@gmail.com>
2019-02-07 17:36:35 +0300

    Fixed some syntax errors

--- app/requests.py
+++ app/requests.py
@@ -4,32 +4,40 @@ api_key=None
 base_url=None
 news_article_url=None
 def configure_request(app):
+
     '''
     A function that takes in the application instance and replaces the value of variables set to none
     '''
+
     global api_key,base_url,news_article_url
     api_key=app.config['API_KEY']
     base_url=app.config['BASE_URL']
     news_article_url=app.config['NEWS_ARTICLE_URL']
 
 def get_news_sources(category):
+
     '''
     A function that gets the json response to our url request
     '''
+
     get_news_sources_url = base_url.format(category,api_key)
     with urllib.request.urlopen(get_news_sources_url) as url:
         get_news_sources_data=url.read()
         get_news_sources_response=json.loads(get_news_sources_data)
         news_sources_results=None
         # print(get_news_sources_response)
+
         if get_news_sources_response['sources']:
             news_sources_results_list=get_news_sources_response['sources']
             news_sources_results=process_sources_results(news_sources_results_list)
     return news_sources_results
 
 def process_sources_results(news_sources_list):
+    
     '''
+
     '''
+
     news_sources_results=[]
     for source in news_sources_list:
         id=source.get('id')
@@ -43,9 +51,11 @@ def process_sources_results(news_sources_list):
     return news_sources_results
 
 def get_news_articles(q):
+
     '''
     A function that returns a json response of news articles
     '''
+
     get_news_articles_url=news_article_url.format(q,api_key)
     with urllib.request.urlopen(get_news_articles_url) as url:
         data=url.read()
