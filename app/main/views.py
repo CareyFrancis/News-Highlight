76304e282e7d38bd777e075d9d5692de3b4c02d2
CareyFrancis <carexfm@gmail.com>
2019-02-07 17:37:36 +0300

    Added a source function that returns the index page

--- app/main/views.py
+++ app/main/views.py
@@ -8,13 +8,11 @@ def index():
     A function that displays the main route page
     '''
     news_sources = get_news_sources('general')
-    sports = get_news_sources('sports')
+    xxx = get_news_sources('sports')
     business = get_news_sources('business')
 
-    # print(news_sources)
-    return render_template('index.html',general=news_sources,sports=sports,business=business)
+    return render_template('index.html',general=news_sources,sports=xxx,business=business)
 
-@main.route('/sources/<id>')
 @main.route('/articles')
 def articles(id):
     '''
@@ -23,3 +21,14 @@ def articles(id):
     source_articles=get_news_articles('id')
 
     return render_template('articles.html',articles=source_articles)
+
+@main.route('/source')
+def source():
+	'''
+	function returns index page and its data
+	'''
+
+	news_sources = get_source('sources')
+
+	return render_template('newssource.html',sources = news_sources)
+
