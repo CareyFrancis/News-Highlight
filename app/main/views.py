241c47a61346d378ac9c5170ca4d817c8d2f8df2
CareyFrancis <carexfm@gmail.com>
2019-02-07 16:37:07 +0300

    Created the views.py file

---
+++ app/main/views.py
@@ -0,0 +1,25 @@
+from flask import render_template,request,redirect,url_for
+from .import main
+from ..requests import get_news_sources,get_news_articles
+from ..models import News_Sources,News_Article
+@main.route('/')
+def index():
+    '''
+    A function that displays the main route page
+    '''
+    news_sources = get_news_sources('general')
+    sports = get_news_sources('sports')
+    business = get_news_sources('business')
+
+    # print(news_sources)
+    return render_template('index.html',general=news_sources,sports=sports,business=business)
+
+@main.route('/sources/<id>')
+@main.route('/articles')
+def articles(id):
+    '''
+    A function that defines the article's objects
+    '''
+    source_articles=get_news_articles('id')
+
+    return render_template('articles.html',articles=source_articles)
